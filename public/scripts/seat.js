4f1d4345903ff7ceb109e24b7de6e506999cda84
Nikita Jiandani <niki.jiandani@gmail.com>
2019-12-04 09:46:18 -0500

    Add markup and fonts

---
+++ public/scripts/seat.js
@@ -0,0 +1,1 @@
+console.log('Welcome to the Seat Lookup App!');
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('Welcome to Seat Lookup');
+  res.send('Welcome to the Seat Lookup API');
 });
 app.listen(port, () => {
   console.log(`App listening on port ${port}. Go to http://localhost:${port}`);
