5ef7b2c25cb58db7933275c30e017104dc66bd0d
Nikita Jiandani <niki.jiandani@gmail.com>
2019-12-04 11:10:58 -0500

    Add people lookup logic to api/people route

--- server/index.js
+++ server/index.js
@@ -1,12 +1,40 @@
 const express = require('express');
+const data = require('./data/MOCK_DATA.json');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Seat Lookup API');
+app.get('/api/people', (req, res) => {
+  let firstName = req.query.firstName || '';
+  let lastName = req.query.lastName || '';
+  let result;
+
+  if (firstName) {
+    result = data.filter(person => person.first_name === firstName);
+    if (lastName) {
+      result = result.filter(person => person.last_name === lastName);
+    }
+  }
+
+  if (lastName) {
+    result = data.filter(person => person.last_name === lastName);
+    if (firstName) {
+      result = result.filter(person => person.first_name === firstName);
+    }
+  }
+
+  if (!result) {
+    res.send(
+      'Welcome to the internal people search directory. You can query this API with firstName and lastName query parameters.'
+    );
+  } else if (result.length > 0) {
+    res.json(result);
+  } else {
+    res.send(`Sorry, cannot find ${firstName} ${lastName} in the system`);
+  }
 });
+
 app.listen(port, () => {
   console.log(`App listening on port ${port}. Go to http://localhost:${port}`);
 });
